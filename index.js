a984b0f38a2da79003d143de805377cacd78030a
Eric-Martin <gaspar.ericmartin@gmail.com>
2023-05-26 15:34:33 -0400

    Complete fetch lab

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books') 
+    .then(response => response.json())
+    .then(function (data) {
+      console.log(data)
+      renderBooks(data)
+      //findCharacterByNumber(data)
+      totalPages(data)
+    })
+    
+    
+  
   
 }
 
@@ -10,6 +20,21 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+}
+
+/*function findCharacterByNumber(books, number= '1031') {
+  for (first of books) {
+    for (character in first.characters) {
+      if (first.characters[character] === `https://anapioficeandfire.com/api/characters/${number}`)
+      console.log(first.characters[character])
+    }
+  }
+
+}*/
+
+function totalPages(books) {
+  const total = books.reduce((pageCount, element) => pageCount + element.numberOfPages, 0)
+  console.log(total)
 }
 
 document.addEventListener('DOMContentLoaded', function() {
